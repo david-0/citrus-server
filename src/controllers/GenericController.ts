443d27e725b5c1e5e623a347cc59fe34ceec5163
David Leuenberger <david.leuenberger@hsr.ch>
2017-12-09 17:52:56 +0100

    remove ModelWrapper, use typeof Model instead

--- src/controllers/GenericController.ts
+++ src/controllers/GenericController.ts
@@ -2,41 +2,40 @@ import * as express from "express";
 import {Logger} from "log4js";
 import {Model} from "sequelize-typescript";
 import {IController} from "./IController";
-import {IModelWrapper} from "./IModelWrapper";
 import log4js = require("log4js");
 
 let LOGGER: Logger = log4js.getLogger("GenericController");
 
 export class GenericController<T extends Model<T>> implements IController {
 
-  constructor(private wrapper: IModelWrapper<T>) {
-    LOGGER = log4js.getLogger(`GenericController-${wrapper.name}`);
+  constructor(private model: typeof Model) {
+    LOGGER = log4js.getLogger(`GenericController-${model.name}`);
   }
 
   public add(req: express.Request, res: express.Response): void {
     const instance: T = req.body;
-    LOGGER.debug(`create ${this.wrapper.name()}: ${JSON.stringify(instance)}`);
-    this.wrapper.create(instance)
+    LOGGER.debug(`create ${this.model.name}: ${JSON.stringify(instance)}`);
+    this.model.create(instance)
       .then((created: T) => {
-        LOGGER.debug(`created ${this.wrapper.name()}. successfully, id: ${created.id}`);
+        LOGGER.debug(`created ${this.model.name}. successfully, id: ${created.id}`);
         res.status(201).json(created);
       })
       .catch((err) => {
-        res.status(500).json({error: `error creating ${this.wrapper.name()} ${instance.id}. ${err}`});
+        res.status(500).json({error: `error creating ${this.model.name} ${instance.id}. ${err}`});
         LOGGER.error("Unable to connect to the database:", err);
       });
   }
 
   public getAll(req: express.Request, res: express.Response): void {
-    this.wrapper.findAll().then((items) => {
+    this.model.findAll().then((items) => {
       res.json(items);
     }).catch((err) => {
-      res.status(404).json({error: `error retrieving all ${this.wrapper.name()}s. ${err}`});
+      res.status(404).json({error: `error retrieving all ${this.model.name}s. ${err}`});
     });
   }
 
   public getRange(req: express.Request, res: express.Response): void {
-    this.wrapper.findAndCountAll({
+    this.model.findAndCountAll({
       limit: req.params.limit,
       offset: req.params.offset,
       order: this.createOrderCondition(req),
@@ -44,25 +43,26 @@ export class GenericController<T extends Model<T>> implements IController {
     }).then((result) => {
       res.json(result);
     }).catch((err) => {
-      res.status(404).json({error: `error retrieving all ${this.wrapper.name()}s. ${err}`});
+      res.status(404).json({error: `error retrieving all ${this.model.name}s. ${err}`});
     });
   }
 
   public get(req: express.Request, res: express.Response): void {
-    this.wrapper.findById(req.params.id)
+//    const options = {include: req.query.include};
+    this.model.findById(req.params.id)
       .then((item) => {
         res.json(item);
       }).catch((err) => {
-      res.status(404).json({error: `error retrieving all ${this.wrapper.name()}s. ${err}`});
+      res.status(404).json({error: `error retrieving all ${this.model.name}s. ${err}`});
     });
   }
 
   public del(req: express.Request, res: express.Response): void {
-    this.wrapper.findById(req.params.id).then((item) => {
+    this.model.findById(req.params.id).then((item) => {
       item.destroy().then(() => {
         res.json(true);
       }).catch((err) => {
-        res.status(404).json({error: `error could not delete ${this.wrapper.name()}. ${err}`});
+        res.status(404).json({error: `error could not delete ${this.model.name}. ${err}`});
       });
     })
       .catch((err) => {
@@ -72,25 +72,26 @@ export class GenericController<T extends Model<T>> implements IController {
 
   public update(req: express.Request, res: express.Response): void {
     const inputItme: T = req.body;
-    LOGGER.debug(`update ${this.wrapper.name()}: ${JSON.stringify(inputItme)}`);
-    this.wrapper.update(inputItme, {where: {id: inputItme.id}})
+    LOGGER.debug(`update ${this.model.name}: ${JSON.stringify(inputItme)}`);
+    this.model.update(inputItme, {where: {id: inputItme.id}})
       .then((result) => {
         if (result[0] === 1) {
           res.json(1);
         } else {
           res.status(404).json(
-            {error: `error update ${this.wrapper.name()} failed (update count is not 1 (${result[0]})`});
+            {error: `error update ${this.model.name} failed (update count is not 1 (${result[0]})`});
         }
       })
       .catch((err) => {
-        res.status(404).json({error: `error update ${this.wrapper.name()} failed. ${err}`});
+        res.status(404).json({error: `error update ${this.model.name} failed. ${err}`});
       });
   }
 
   private createFilterCondition(req: express.Request): any {
-    if (req.query && req.query.filter && this.wrapper.filterColumns().length > 0) {
+    if (req.query && req.query.filter && req.query.columns.length > 0) {
+      const filterColumns: string[] = [];
       const or = {};
-      this.wrapper.filterColumns().forEach((columnName) => {
+      filterColumns.forEach((columnName) => {
         or[columnName] = {$like: `%${req.query.filter}%`};
       });
       return {$or: or};
