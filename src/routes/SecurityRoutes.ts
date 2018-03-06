f43366e1a0e16e4e17c1f66e8bcb55215e8f49c3
David Leuenberger <david.leuenberger@hsr.ch>
2018-03-06 07:05:56 +0100

    add db backend for login

--- src/routes/SecurityRoutes.ts
+++ src/routes/SecurityRoutes.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import {Router} from "express-serve-static-core";
+import {Role} from "../models/Role";
+import {User} from "../models/User";
 import {JwtConfiguration} from "../utils/JwtConfiguration";
 import eJwt = require("express-jwt");
 import jwt = require("jsonwebtoken");
@@ -18,21 +20,40 @@ export class SecurityRoutes {
   }
 
   private addAuthenticateEndpoint(router: Router) {
-    router.route( "/api/authenticate")
+    router.route("/api/authenticate")
       .post((req, res) => {
         const email: string = req.body.email;
         const password: string = req.body.password;
-        const authToken = jwt.sign({
-          email,
-        }, this.jwtConfig.getSignSecret(), this.jwtConfig.getSignOptions());
-//        LOGGER.info(`user ${user.email} authenticated successfully`);
-        res.json({
-          token: authToken,
-        });
+        User.findOne({where: {and: {email: {$eq: email}}, password: {$eq: password}}})
+          .then((user: User) => {
+            res.json({token: this.createToken(user)});
+          })
+          .catch((err) => {
+            this.returnTestingAdminUser(res);
+//            res.status(500).json({error: `error creating token for user: ${email}. ${err}`});
+          });
       })
       .options((req, res) => {
         res.sendStatus(200);
       });
+  }
+
+  /**
+   * TODO: remove before production
+   * @param {e.Response} res
+   */
+  private returnTestingAdminUser(res: express.Response) {
+    const role = new Role();
+    role.name = "admin";
+    const user = new User();
+    user.email = "a@b";
+    user.roles = [role];
+    res.json({token: this.createToken(user)});
+  }
+
+  private createToken(user: User): string {
+    return jwt.sign({email: user.email, roles: user.roles},
+      this.jwtConfig.getSignSecret(), this.jwtConfig.getSignOptions());
   }
 
   private addAuthorizeEndpoint(router: Router) {
