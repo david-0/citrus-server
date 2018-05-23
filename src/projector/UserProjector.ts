25f5fc1e3bfff861a109918795bbb15e9ecf8584
David Leuenberger <david.leuenberger@hsr.ch>
2018-05-23 17:05:18 +0200

    update common to 0.2.0

--- src/projector/UserProjector.ts
+++
@@ -1,8 +0,0 @@
-import {IBeforeUpdate} from "../controllers/IBeforeInterface";
-import {Address} from "../models/Address";
-import {User} from "../models/User";
-
-export class UserProjector implements IBeforeUpdate<User> {
-  public beforeUpdate(instance: User) {
-  }
-}
