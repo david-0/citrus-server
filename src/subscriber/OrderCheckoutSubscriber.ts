cc2f5e8a6dc3141448fa746fdedc23330b505cb3
David Leuenberger <david.leuenberger@hsr.ch>
2018-11-16 07:57:20 +0100

    update quantities on checking out (or disable checking out)

--- src/subscriber/OrderCheckoutSubscriber.ts
+++ src/subscriber/OrderCheckoutSubscriber.ts
@@ -1,4 +1,5 @@
-import {EntitySubscriberInterface, EventSubscriber, InsertEvent, UpdateEvent} from "typeorm";
+import {EntityManager, EntitySubscriberInterface, EventSubscriber, InsertEvent, UpdateEvent} from "typeorm";
+import {ArticleStock} from "../entity/ArticleStock";
 import {Order} from "../entity/Order";
 import {User} from "../entity/User";
 
@@ -13,9 +14,11 @@ export class OrderCheckoutSubscriber implements EntitySubscriberInterface<Order>
       const user = await event.manager.getRepository(User).findOne(event.queryRunner.data);
       event.entity.checkedOutDate = new Date();
       event.entity.checkingOutUser = user;
+      await this.updateStockQuantitiesOnCheckout(event.entity, event.manager);
     } else if (event.databaseEntity.checkedOut && !event.entity.checkedOut) {
       event.entity.checkedOutDate = null;
       event.entity.checkingOutUser = null;
+      await this.updateStockQuantitiesOnDisableCheckout(event.entity, event.manager);
     }
   }
 
@@ -24,6 +27,68 @@ export class OrderCheckoutSubscriber implements EntitySubscriberInterface<Order>
       const user = await event.manager.getRepository(User).findOne(event.queryRunner.data);
       event.entity.checkedOutDate = new Date();
       event.entity.checkingOutUser = user;
+      await this.updateStockQuantitiesOnCheckout(event.entity, event.manager);
     }
+  }
+
+  private async updateStockQuantitiesOnCheckout(order: Order, manager: EntityManager) {
+    const completeOrder = await this.loadOrderItems(manager, order);
+    if (completeOrder.orderItems) {
+      for (const item of completeOrder.orderItems) {
+        const articleStock = await this.loadArticleStock(manager, item.article.id, order.location.id);
+        articleStock.reservedQuantity -= item.quantity;
+        await manager.getRepository(ArticleStock).save(articleStock);
+      }
+    }
+    if (completeOrder.checkedOutOrderItems) {
+      for (const checkedOutItem of completeOrder.checkedOutOrderItems) {
+        const articleStock = await this.loadArticleStock(manager, checkedOutItem.article.id, order.location.id);
+        articleStock.quantity += checkedOutItem.quantity;
+        await manager.getRepository(ArticleStock).save(articleStock);
+      }
+    }
+  }
+
+  private async updateStockQuantitiesOnDisableCheckout(order: Order, manager: EntityManager) {
+    const completeOrder = await this.loadOrderItems(manager, order);
+    if (completeOrder.orderItems) {
+      for (const item of completeOrder.orderItems) {
+        const articleStock = await this.loadArticleStock(manager, item.article.id, order.location.id);
+        articleStock.reservedQuantity += item.quantity;
+        await manager.getRepository(ArticleStock).save(articleStock);
+      }
+    }
+    if (completeOrder.checkedOutOrderItems) {
+      for (const checkedOutItem of completeOrder.checkedOutOrderItems) {
+        const articleStock = await this.loadArticleStock(manager, checkedOutItem.article.id, order.location.id);
+        articleStock.quantity -= checkedOutItem.quantity;
+        await manager.getRepository(ArticleStock).save(articleStock);
+      }
+    }
+  }
+
+  private async loadArticleStock(manager: EntityManager, articleId: number, locationId: number): Promise<ArticleStock> {
+    return (await manager.getRepository(ArticleStock)
+      .find({
+        where:
+          {
+            article: {id: articleId},
+            location: {id: locationId},
+          },
+      }))[0];
+  }
+
+  private async loadOrderItems(manager: EntityManager, order: Order) {
+    const completeOrder = await manager.getRepository(Order).findOne(order.id,
+      {
+        relations: [
+          "location",
+          "orderItems",
+          "orderItems.article",
+          "checkedOutOrderItems",
+          "checkedOutOrderItems.article",
+        ],
+      });
+    return completeOrder;
   }
 }
