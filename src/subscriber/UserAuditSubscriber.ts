a2ec460dd130d965b97d85ba3fd71e12d8bed8bd
David Leuenberger <david.leuenberger@gmx.ch>
2019-03-13 17:05:16 +0100

    add UserAuditSubscriber

---
+++ src/subscriber/UserAuditSubscriber.ts
@@ -0,0 +1,21 @@
+import {EntitySubscriberInterface, EventSubscriber, InsertEvent} from "typeorm";
+import {UserAudit} from "../entity/UserAudit";
+
+@EventSubscriber()
+export class UserAuditSubscriber implements EntitySubscriberInterface<UserAudit> {
+  public listenTo() {
+    return UserAudit;
+  }
+
+  public async beforeInsert(event: InsertEvent<UserAudit>) {
+    if (event.queryRunner.data && event.queryRunner.data.ip && event.queryRunner.data.ips) {
+      const ips: string[] = [event.queryRunner.data.ip];
+      ips.push(...event.queryRunner.data.ips);
+      event.entity.ip = ips.join(", ");
+    }
+    if (event.queryRunner.data && event.queryRunner.data.headers) {
+      event.entity.userAgent = event.queryRunner.data.headers["user-agent"];
+    }
+    event.entity.date = new Date();
+  }
+}
