836d18b1d4dec094ece650dee0a99b8b29ba3716
David Leuenberger <david.leuenberger@gmx.ch>
2019-03-13 17:33:13 +0100

    try to send mails only if initialized

--- src/utils/MailService.ts
+++ src/utils/MailService.ts
@@ -1,13 +1,17 @@
 import * as fs from "fs";
+import {getLogger, Logger} from "log4js";
 import * as nodemailer from "nodemailer";
 import {SentMessageInfo} from "nodemailer";
 
 export class MailService {
+  private LOGGER: Logger = getLogger("Server");
+
   private host: string;
   private port: number;
   private user: string;
   private password: string;
-  private from: string
+  private from: string;
+  private initialized: boolean;
 
   constructor(configFile: string) {
     if (fs.existsSync(configFile)) {
@@ -18,25 +22,32 @@ export class MailService {
       this.user = config.user;
       this.password = config.password;
       this.from = config.from;
+      this.initialized = true;
+      this.LOGGER.info("Mailservice initialized");
+    } else {
+      this.LOGGER.warn("Mailservice not initialized: no configfile found.");
     }
   }
 
-  public async sendMail(to: string, subject: string, text: string, html: string): Promise<SentMessageInfo> {
-    const transporter = nodemailer.createTransport({
-      host: this.host,
-      port: this.port,
-      auth: {
-        user: this.user,
-        pass: this.password
-      },
-    });
-    return await transporter.sendMail({
-      from: this.from,
-      to,
-      subject,
-      text,
-      html,
-    });
+  public async sendMail(to: string, subject: string, text: string, html: string): Promise<SentMessageInfo | undefined> {
+    if (this.initialized) {
+      const transporter = nodemailer.createTransport({
+        auth: {
+          pass: this.password,
+          user: this.user,
+        },
+        host: this.host,
+        port: this.port,
+      });
+      return await transporter.sendMail({
+        from: this.from,
+        html,
+        subject,
+        text,
+        to,
+      });
+    }
+    return undefined;
   }
 
 }
