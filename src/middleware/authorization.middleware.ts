5720b8bbc627bf0e0ed9cd709f989b2b452e28c7
David <david.leuenberger@gmx.ch>
2024-01-30 01:13:54 +0100

    remove db call during login

--- src/middleware/authorization.middleware.ts
+++ src/middleware/authorization.middleware.ts
@@ -1,17 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "../utils/app-data-source";
-import { User } from "../entity/User";
 
 export const authorization = (roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     return await AppDataSource.transaction(async (manager) => {
-      const userRepo = manager.getRepository(User);
-      const user = await userRepo.findOne({
-        where: { id: req["currentUser"].id },
-        relations: ["roles"],
-      });
+      const user = req["currentUser"];
       if (!(user && (!roles.length ||
-        roles.filter(role => user.roles.map(r => r.name).indexOf(role) !== -1).length > 0))) {
+        roles.filter(role => user.roles.indexOf(role) !== -1).length > 0))) {
         return res.status(403).json({ message: "Forbidden" });
       }
       next();
